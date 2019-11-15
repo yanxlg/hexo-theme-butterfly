a9a5c514290bcc178624bef825647699c23a76fc
yanxianliang <798410225@qq.com>
2019-11-15 22:15:38 +0800

    Update badge.js

--- source/js/badge.js
+++ source/js/badge.js
@@ -1,1 +1,21 @@
-document
+document.addEventListener("ready",()=>{
+
+const script = document.querySelector("script[bagde]");
+
+if(script){
+let tags=script.attributes.tags;
+try{
+tags=Json.parse(tags);
+for(tag of tags){
+const {tag,}
+}
+}catch(e){
+
+}
+
+}
+
+
+
+
+})
