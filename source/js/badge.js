323a977f7890071fffdd4d8212052590cd77e734
yanxlg <xlyan@vova.com.hk>
2019-11-16 09:48:31 +0800

    add badge.js

--- source/js/badge.js
+++ source/js/badge.js
@@ -1,21 +1,30 @@
-document.addEventListener("ready",()=>{
+window.addEventListener("load", () => {
+    const script = document.querySelector("script[bagde]");
+    if (script) {
+        let tags = script.attributes.tags;
+        try {
+            tags = JSON.parse(tags);
+            for (const _tag of tags) {
+                const {tag,label,url,color="blue",dataPath,dataTemplate} = _tag;
+                const img = document.querySelector(`[alt='${tag}']`);
+                if(img&&url){
+                    fetch(url).then((result)=>{
+                        const paths = dataPath.split(".");
+                        try{
+                            let data=result;
+                            for (const path of paths){
+                                data = data[path];
+                            }
+                            const value = dataTemplate?dataTemplate.replace("$data",data):data;
+                            img.src=`https://img.shields.io/badge/${label}-${value}-${color})`;
+                        }catch (e) {
 
-const script = document.querySelector("script[bagde]");
+                        }
+                    });
+                }
+            }
+        } catch (e) {
 
-if(script){
-let tags=script.attributes.tags;
-try{
-tags=Json.parse(tags);
-for(tag of tags){
-const {tag,}
-}
-}catch(e){
-
-}
-
-}
-
-
-
-
-})
+        }
+    }
+});
